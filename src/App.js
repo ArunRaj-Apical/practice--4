e42cf7597fabb5f666d6d60ac178c1f7fef5cbb3
ramasuryagarigipati-apical <ramasuryag@apicalsoftsolution.com>
2026-02-27 09:55:50 +0530

    Save HMS project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DoctorRegistrationForm from './Components/RegistrationPages/DoctorRegistrationForm'
+
+function App() {
+  return (
+    <div className="App">
+      <DoctorRegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
